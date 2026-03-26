fixed r profile calculation

dense_plume_analysis.py
@@ -42,16 +42,6 @@ def centerline_analysis_buoyancy(bw_fluc_center, dbdz_center, z, nx):
     # finding Ozmidov length scale of plume
     dbdz_plume_avg = np.mean(dbdz_center[max_index:])
 
-    # info at max tracer height
-    #max_w = w_center[max_index]
-    #max_b_fluc = b_fluc_center[max_index]
-    #max_rho_perturbed = rho_perturbed_center[max_index]
-
-    # info at neutral buoyancy height
-    #neutral_w = w_center[neutral_index]
-    #neutral_b_fluc = b_fluc_center[neutral_index]
-    #neutral_rho_perturbed = rho_perturbed_center[neutral_index]
-
     return neutral_index, max_index, dbdz_plume_avg
 
 def plume_bw_anlaysis(w, tracer, b_perturbed, bw_perturbed, rho_perturbed, nx, contour, centerline_index, r_max_profile, plume_index):
@@ -120,7 +110,7 @@ def plume_bw_anlaysis(w, tracer, b_perturbed, bw_perturbed, rho_perturbed, nx, c
     idx_rp_max = idx_rp_max[np.where(neural_vs_rp==np.min(neural_vs_rp))] 
     return idx_rp_max, idx_neutral_in_plume, idx_neutral_in_plume
 
-def plume_tracer_analysis(x, y, z, lx, nx, tracer, contour, calc_option='middle domain'):
+def plume_tracer_analysis(x, y, z, lx, nx, tracer, idx, calc_option='middle domain'):
     if calc_option == 'middle domain':
         # finding centerline of plume 
         centerline_index = np.zeros((3, nx[2])).astype(int)
@@ -148,10 +138,9 @@ def plume_tracer_analysis(x, y, z, lx, nx, tracer, contour, calc_option='middle 
             center_xy_loc[2, k] = z[k]
         centerline_index = np.round(centerline_index).astype(int)
     # finding plume bounds via contour on the centerline of the tracer
-    tracer_bnd = tracer[centerline_index[0, :], centerline_index[1, :], centerline_index[2, :]]*contour
-    plume_contour = np.zeros_like(tracer).astype(bool)
-    for k in range(nx[2]):
-        plume_contour[:, :, k] = tracer[:, :, k] >= tracer_bnd[k]
+    contour = 0.05
+    tracer_contour = tracer[centerline_index[0, idx], centerline_index[0, idx], idx]*contour
+    plume_contour = tracer >= tracer_contour
     plume_index = np.where(plume_contour)
     edge_mask = plume_contour & (
         ~np.roll(plume_contour, 1, axis=0)

oceananigans_T_nbp_big_memory.py
@@ -50,6 +50,8 @@ rho0 = 1026
 T0 = 25 
 S0 = 0 
 Sj = 0.1
+F_s = Sj*0.001
+rj = 10
 # plotting prep
 # font for plotting 
 plt.rcParams['font.family'] = 'serif' # or 'sans-serif' or 'monospace'
@@ -160,9 +162,9 @@ for it in nt:
         drhodz = np.gradient(rho, z, axis=-1)
         b = -g*alpha*(T - T0)
     else:
-        rhoS = rho0 + rho0 * beta * (S - S0)
-        rhoT = rho0 - rho0 * alpha * (T - T0)
-        rho = rho0 - rho0 * alpha * (T - T0)+ rho0 * beta * (S - S0)
+        rhoS = rho0 * beta * (S - S0)
+        rhoT = - rho0 * alpha * (T - T0)
+        rho = rho0 + rhoS + rhoT
         drhodz = np.gradient(rho, z, axis=-1)
         b = g*alpha*(T - T0) - g*beta*(S - S0)
     # interpolate so all values are from the center, center, center of the grid cell
@@ -235,7 +237,8 @@ for it in nt:
         db_flucdz_avg = np.mean(db_flucdz, axis=(-3, -2))
         
         if salinity:
-            center_xy_loc, centerline_index, rp_profile, plume_index = plume_tracer_analysis(x, y, z, lx, nx, S, 0.05)
+            bw_idx = np.where(bw_fluc_avg==np.max(bw_fluc_avg))[0][0]
+            center_xy_loc, centerline_index, rp_profile, plume_index = plume_tracer_analysis(x, y, z, lx, nx, S, bw_idx)
             dbdx = np.mean(np.gradient(b, x, axis=0), axis=(-3, -2))
             dbdy = np.mean(np.gradient(b, y, axis=1), axis=(-3, -2))
             dbdz =  np.mean(np.gradient(b, z, axis=2), axis=(-3, -2))

spatial_comparisons_oceanananigans.py
@@ -59,7 +59,7 @@ T0 = 25.0
 S0 = 0 
 wp = 0.001
 Sj = np.array([0.05, 0.1, 0.15, 0.2])# 0.1 * np.ones(num_cases) # 
-Sflux = np.dot(Sj, wp)
+F_s = np.dot(Sj, wp)
 if np.size(Sj) == 1:
     contour = np.dot(Sj * np.ones(num_cases), 0.05)
 else:
@@ -121,7 +121,7 @@ else:
 
 z = (z*np.ones([num_cases, nx[2]])).T
 zf = (zf*np.ones([num_cases, nx[2] + 1])).T
-Sj = Sflux / (np.sqrt(g  * rj))
+Sj = F_s / (np.sqrt(g  * rj))
 
 ############ NONDIMENSIONALIZATION ############
 if ND: 
@@ -136,7 +136,7 @@ if ND:
         N2[i] = g  * dTdz[i] / T0
         b_scale[i] = mld[i] * N2[i]
         vel_scale[i] = mld[i] * np.sqrt(N2[i])
-        Sj[i] = Sflux[i]/ (vel_scale[i]) # / (np.sqrt(g  * rj)) #
+        Sj[i] = F_s[i]/ (vel_scale[i]) # / (np.sqrt(g  * rj)) #
         z_nd[:, i] = z[:, i] / mld[i]
         zf_nd[:, i] = zf[:, i] / mld[i]
         bflux_scale = b_scale[i] * vel_scale[i]

temporal_comparisons_oceananigans.py
@@ -49,7 +49,7 @@ T0 = 25
 S0 = 0 
 wp = 0.001
 Sj = np.array([0.05, 0.1, 0.15, 0.2]) #0.1 * np.ones(num_cases) # 
-Sflux = np.dot(Sj, wp)
+F_s = np.dot(Sj, wp)
 contour = np.dot(Sj, 0.05)
 # plotting prep
 color_opt, line_opt = plot_format(num_cases)
@@ -229,7 +229,7 @@ if ND:
         N2[i] = g  * dTdz[i] / T0
         b_scale[i] = mld[i] * N2[i]
         vel_scale[i] = mld[i] * np.sqrt(N2[i])
-        Sj[i] = Sflux[i] / (vel_scale[i]) #/ (np.sqrt(g  * rj)) #
+        Sj[i] = F_s[i] / (vel_scale[i]) #/ (np.sqrt(g  * rj)) #
         z[i, :] = z[i, :] / mld[i]
         zf[i, :] = zf[i, :] / mld[i]
     bflux_scale = b_scale * vel_scale