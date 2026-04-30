fixed bugs from major update. included a setup script for new models that need temporal averages

comparisons_oceanananigans.py
@@ -85,7 +85,7 @@ x = readers[0].x
 y = readers[0].y
 
 # physical parameters
-centerx = 0.0
+x0 = 0.0
 centery = 0.0
 rj = 5 # m, radius of salinity flux circle at the surface
 g = 9.80665  # gravity in m/s^2
@@ -287,7 +287,7 @@ else:
             # interpolate velocities to cell centers
             u, v, w = velocities_to_center(u, v, w)
             # convert temperature and salinity to buoyancy 
-            bs = buoyancy(T, rho0, coeffs, T0, g, tracer = S if salinity else None)
+            bs = buoyancy(T, rho0, coeffs, T0, tracer = S if salinity else None)
             b = bs['b_total']
             rho = bs['rho']
             rho_fluc = rho - np.mean(rho, axis=(-3, -2))
@@ -318,9 +318,9 @@ else:
                     dense_plume[i].b_background = g*alpha*(T - T0)
                     r_profile.append(dense_plume[i].plume_tracer_radius(x, y))
 
-                    b_center.append(vertical_line(b, x, y, centerx, centery))
-                    T_fluc_center.append(vertical_line(T-T_avg[i], x, y, centerx, centery))
-                    S_fluc_center.append(vertical_line(S-S_avg[i], x, y, centerx, centery))
+                    b_center.append(vertical_line(b, x, y, x0, centery))
+                    T_fluc_center.append(vertical_line(T-T_avg[i], x, y, x0, centery))
+                    S_fluc_center.append(vertical_line(S-S_avg[i], x, y, x0, centery))
             # horizontal lines to save for plotting
             if plot_1d_y:
                 u_hor.append(horizontal_line(u, y, z[i], centery, loc_z[i]))
@@ -334,15 +334,15 @@ else:
                 S_hor.append(horizontal_line(S, y, z[i], centery, loc_z[i]))
             # plane slices to save for plotting
             if plot_variables and planeslice == 'vertical':
-                #T_plane.append(yz_plane(T, x, centerx))
-                u_plane.append(yz_plane(u, x, centerx))
-                #v_plane.append(yz_plane(v, x, centerx))
-                w_plane.append(yz_plane(w, x, centerx))
-                #rho_plane.append(yz_plane(rho, x, centerx))
-                bw_plane.append(yz_plane(bw_fluc, x, centerx))
-                rho_fluc_plane.append(yz_plane(rho_fluc, x, centerx))
+                #T_plane.append(yz_plane(T, x, x0))
+                u_plane.append(yz_plane(u, x, x0))
+                #v_plane.append(yz_plane(v, x, x0))
+                w_plane.append(yz_plane(w, x, x0))
+                #rho_plane.append(yz_plane(rho, x, x0))
+                bw_plane.append(yz_plane(bw_fluc, x, x0))
+                rho_fluc_plane.append(yz_plane(rho_fluc, x, x0))
                 if salinity:
-                    S_plane.append(yz_plane(S, x, centerx))
+                    S_plane.append(yz_plane(S, x, x0))
             elif plot_variables and planeslice == 'horizontal':
                 T_plane.append(xy_plane(T, z, loc_z[i]))
                 u_plane.append(xy_plane(u, z, loc_z[i]))

dense_plume.py
@@ -4,7 +4,7 @@ from scipy.spatial import ConvexHull
 
 from interpolation import vertical_line
 class PlumeAnalysis:
-    def __init__(self, tracer_contour):
+    def __init__(self, tracer_contour = None):
         self.tracer = None 
         self.tracer_contour = tracer_contour
 
@@ -35,11 +35,12 @@ class PlumeAnalysis:
         self._contour_cache = {}
 
     ### -------------------------TRACER INFORMATION------------------------- ###
-    def input_tracer(self, tracer, b_tracer, b_background, bw_fluc):
+    def input_info(self, tracer, b_tracer = None, b_background = None, bw_fluc = None):
         self.tracer = tracer
         self.b_tracer = b_tracer
         self.b_background = b_background
-        self.b_fluc = b_background - b_tracer
+        if b_tracer is not None and b_background is not None:
+            self.b_fluc = b_background - b_tracer
         self.b_fluc_w = bw_fluc
         self.nz = self.tracer.shape[-1]
 
@@ -65,16 +66,17 @@ class PlumeAnalysis:
         xi, yi, zi = np.where(plume_contour)
         self.plume_index = [xi, yi, zi]
 
-        centerx = np.mean(x)
+        x0 = np.mean(x)
         centery = np.mean(y)
 
-        r = np.sqrt((x[xi] - centerx)**2 + (y[yi] - centery)**2)
+        r = np.sqrt((x[xi] - x0)**2 + (y[yi] - centery)**2)
         counts = np.bincount(zi, minlength=self.nz)
         sums   = np.bincount(zi, weights=r, minlength=self.nz)
 
         self.radius_tracer = np.zeros(self.nz)
         mask = counts > 0
         self.radius_tracer[mask] = sums[mask] / counts[mask]
+        return self.radius_tracer
 
     ### -------------------------MOMENTUM ANALYSIS------------------------- ###
     def plume_momentum_analysis(self, w, b):

diagnostics.py
@@ -2,6 +2,10 @@ import numpy as np
 import os 
 import h5py
 
+from interpolation import velocities_to_center, vertical_line, point
+from dense_plume import PlumeAnalysis
+from physics import rms, buoyancy
+
 ### -------------------------COLLECTING COMPARISON CASE INFO------------------------- ###
 def comparison_info(variations, universal_folder, ND=False):
     if variations == 'strat':
@@ -88,16 +92,24 @@ def comparison_info(variations, universal_folder, ND=False):
         }
     return case_info
 ### -------------------------CALCULATING TEMPORAL AVERAGES------------------------- ###
-def compute_temporal_averages(reader, time_indices, grid, physics, interp, center=(0.0, 0.0)):
-    x, y, z = grid["x"], grid["y"], grid["z"]
-    nx = grid["nx"]
+def compute_temporal_averages(reader, center=(0.0, 0.0), start=10, T0 = 25.0, rho0 = 1026.0):
+    x, y, z = reader.x, reader.y, reader.z
+    nt = reader.nt
+    t_save = reader.t_save
+    nx = reader.nx
     x0, y0 = center
+
+    coeffs = reader.load_equation_of_state(True)
+
     n = 0
     # ---------------- initializing arrays ---------------- #
     S_avg = np.zeros(nx[2])
     T_avg = np.zeros(nx[2])
     b_avg = np.zeros(nx[2])
     w_avg = np.zeros(nx[2])
+
+    T_fluc_avg = np.zeros(nx[2])
+    b_fluc_avg = np.zeros(nx[2])
 
     Tw_avg = np.zeros(nx[2])
     Sw_avg = np.zeros(nx[2])
@@ -107,87 +119,145 @@ def compute_temporal_averages(reader, time_indices, grid, physics, interp, cente
     v_rms = np.zeros(nx[2])
     w_rms = np.zeros(nx[2])
 
+    S_value = 0
+    w_value = 0
+
     S_center = np.zeros(nx[2])
     T_center = np.zeros(nx[2])
+    T_fluc_center = np.zeros(nx[2])
     b_center = np.zeros(nx[2])
+    b_fluc_center = np.zeros(nx[2])
     w_center = np.zeros(nx[2])
 
     # ---------------- time loop ---------------- #
-    for it in time_indices:
-        u, v, w, T, S, _, _ = reader(it)
+    for it in range(start, nt):
+        u = reader.lazy_field('u', t_save[it])
+        v = reader.lazy_field('v', t_save[it])
+        w = reader.lazy_field('w', t_save[it])
+        T = reader.lazy_field('T', t_save[it])
+        S = reader.lazy_field('S', t_save[it])
         # center velocities
-        u = interp.fcc_ccc(u)
-        v = interp.cfc_ccc(v)
-        w = interp.ccf_ccc(w)
-
+        u, v, w = velocities_to_center(u, v, w)
         # buoyancy
-        b = (
-            physics["g"] * physics["alpha"] * (T - physics["T0"])
-            - physics["g"] * physics["beta"] * (S - physics["S0"])
-        )
+        b_temp = buoyancy(T, rho0, coeffs, T0, S)
+        b = b_temp['b_total']
 
         # ---------------- horizontal means ---------------- #
-        S_h = np.mean(S, axis=(-3, -2))
-        T_h = np.mean(T, axis=(-3, -2))
-        b_h = np.mean(b, axis=(-3, -2))
-        w_h = np.mean(w, axis=(-3, -2))
+        S_xy = np.mean(S, axis=(-3, -2))
+        T_xy = np.mean(T, axis=(-3, -2))
+        b_xy = np.mean(b, axis=(-3, -2))
+        w_xy = np.mean(w, axis=(-3, -2))
         # ---------------- fluctuations ---------------- #
-        S_fluc = S - S_h
-        T_fluc = T - T_h
-        b_fluc = b - b_h
-        w_fluc = w - w_h
+        T_fluc = T - T_xy
+        b_fluc = b - b_xy
+        T_fluc_xy = np.mean(T_fluc, axis=(-3, -2))
+        b_fluc_xy = np.mean(b_fluc, axis=(-3, -2))
         # ---------------- fluxes ---------------- #
-        Tw = T_fluc * w_fluc
-        Sw = S_fluc * w_fluc
-        bw = b_fluc * w_fluc
+        Tw = np.mean(T_fluc * w, axis=(-3, -2))
+        Sw = np.mean(S * w, axis=(-3, -2))
+        bw = np.mean(b_fluc * w, axis=(-3, -2))
         # ---------------- RMS ---------------- #
-        u_rms += np.mean((u - np.mean(u, axis=(-3, -2)))**2, axis=(-3, -2))
-        v_rms += np.mean((v - np.mean(v, axis=(-3, -2)))**2, axis=(-3, -2))
-        w_rms += np.mean(w_fluc**2, axis=(-3, -2))
+        u_rms += rms(u)
+        v_rms += rms(v)
+        w_rms += rms(w)
+        # ---------------- contour values ---------------- #
+        bw_idx = np.where(bw==np.max(bw))[0][0]
+        S_value += point(S, x, y, z, x0, y0, z[bw_idx])
+        w_value += point(w, x, y, z, x0, y0, z[bw_idx])
         # ---------------- accumulation ---------------- #
-        S_avg += S_h
-        T_avg += T_h
-        b_avg += b_h
-        w_avg += w_h
-        Tw_avg += np.mean(Tw, axis=(-3, -2))
-        Sw_avg += np.mean(Sw, axis=(-3, -2))
-        bw_avg += np.mean(bw, axis=(-3, -2))
-        # ---------------- centerline ---------------- #
-        S_center += interp.xy_plane(S, z, z0=0.0)
-        T_center += interp.xy_plane(T, z, z0=0.0)
-        b_center += interp.xy_plane(b, z, z0=0.0)
-        w_center += interp.xy_plane(w, z, z0=0.0)
+        S_avg += S_xy
+        T_avg += T_xy
+        b_avg += b_xy
+        w_avg += w_xy
+        T_fluc_avg += T_fluc_xy
+        b_fluc_avg += b_fluc_xy
+        Tw_avg += Tw
+        Sw_avg += Sw
+        bw_avg += bw
+        S_center += vertical_line(S, x, y, x0, y0)
+        T_center += vertical_line(T, x, y, x0, y0)
+        T_fluc_center += vertical_line(T_fluc, x, y, x0, y0)
+        b_center += vertical_line(b, x, y, x0, y0)
+        b_fluc_center += vertical_line(b_fluc, x, y, x0, y0)
+        w_center += vertical_line(w, x, y, x0, y0)
 
         n += 1
 
-    inv_n = 1.0 / n
-
     return {
-        "S_avg": S_avg * inv_n,
-        "T_avg": T_avg * inv_n,
-        "b_avg": b_avg * inv_n,
-        "w_avg": w_avg * inv_n,
+        "S_avg": S_avg / n,
+        "T_avg": T_avg / n,
+        "b_avg": b_avg / n,
+        "w_avg": w_avg / n,
 
-        "Tw_avg": Tw_avg * inv_n,
-        "Sw_avg": Sw_avg * inv_n,
-        "bw_avg": bw_avg * inv_n,
+        "T_fluc_avg": T_fluc_avg / n,
+        "b_fluc_avg": b_fluc_avg / n,
 
-        "u_rms": np.sqrt(u_rms * inv_n),
-        "v_rms": np.sqrt(v_rms * inv_n),
-        "w_rms": np.sqrt(w_rms * inv_n),
+        "Tw_avg": Tw_avg / n,
+        "Sw_avg": Sw_avg / n,
+        "bw_avg": bw_avg / n,
 
-        "S_center": S_center * inv_n,
-        "T_center": T_center * inv_n,
-        "b_center": b_center * inv_n,
-        "w_center": w_center * inv_n,
+        "u_rms": u_rms / n,
+        "v_rms": v_rms / n,
+        "w_rms": w_rms / n,
+
+        "S_center": S_center / n,
+        "T_center": T_center / n,
+        "T_fluc_center": T_fluc_center / n,
+        "b_center": b_center / n,
+        "b_fluc_center": b_fluc_center / n,
+        "w_center": w_center / n,
+
+        "S_value": S_value / n,
+        "w_value": w_value / n
     }
+
+def compute_temporal_radius_avg(reader, tracer0, contour_bound = 0.05, start = 10):
+    dense_plume = PlumeAnalysis(tracer0*contour_bound)
+    x, y = reader.x, reader.y
+    nx = reader.nx
+    nt = reader.nt
+    t_save = reader.t_save
+    # ---------------- initializing arrays ---------------- #
+    r_avg = np.zeros(nx[2])
+    n = 0
+    # ---------------- time loop ---------------- #
+    for it in range(start, nt):
+        S = reader.lazy_field('S', t_save[it])
+        dense_plume.input_info(S)
+        r = dense_plume.plume_tracer_radius(x, y)
+        r_avg += r
+        n += 1
+
+    return r_avg / n
 ### -------------------------WRITING TEMPORAL AVERAGES------------------------- ###
-def write_temporal_averages(file_path, data):
+def write_temporal_averages(file_path, data, contour_bound = 0.05):
+    folder_avg = "1D temporal averages"
+    folder_centerline = "centerline temporal averages"
+    folder_contour = f"contour temporal averages"
+    folder_plume = f"plume statistics/contour {contour_bound}"
+
     with h5py.File(file_path, "w") as f:
-        grp1 = f.create_group("1D averages")
-        grp2 = f.create_group("centerline")
-        for k, v in data.items():
-            if "center" in k:
-                grp2.create_dataset(k, data=v)
-            else:
-                grp1.create_dataset(k, data=v)
+        f.create_group(f'{folder_avg}')
+        f.create_dataset(f'{folder_avg}/S', data=data['S_avg'])
+        f.create_dataset(f'{folder_avg}/T', data=data['T_avg'])
+        f.create_dataset(f'{folder_avg}/b', data=data['b_avg'])
+        f.create_dataset(f'{folder_avg}/w', data=data['w_avg'])
+        f.create_dataset(f'{folder_avg}/T\'', data=data['T_fluc_avg'])
+        f.create_dataset(f'{folder_avg}/b\'w', data=data['bw_fluc_avg'])
+
+        f.create_group(f'{folder_centerline}')
+        f.create_dataset(f'{folder_centerline}/S', data=data['S_center'])
+        f.create_dataset(f'{folder_centerline}/T', data=data['T_center'])
+        f.create_dataset(f'{folder_centerline}/T\'', data=data['T_fluc_center'])
+        f.create_dataset(f'{folder_centerline}/b', data=data['b_center'])
+        f.create_dataset(f'{folder_centerline}/b\'', data=data['b_fluc_center'])
+        f.create_dataset(f'{folder_centerline}/w', data=data['w_center'])
+
+        f.create_group(f'{folder_contour}')
+        f.create_dataset(f'{folder_contour}/S', data=data['S_value'])
+        f.create_dataset(f'{folder_contour}/w', data=data['w_value'])
+
+        f.create_group(f'{folder_plume}')
+        f.create_dataset(f'{folder_plume}/plume tracer radius with depth', data=data['radius_tracer'])
+
+

oceananigans_T.py
@@ -1,16 +1,16 @@
 import os
 import re
 import numpy as np
-import matplotlib.pyplot as plt
 
 from plotting_functions import plot_format, plot_ranges, create_video, plot_momentum_plume, plot_tracer_plume, vert_plane_slices, xy_plane_slices, buoyancy_analysis_plot, turb_stats_plot
 
-from .reader import OceananigansData
-from .physics import reynolds_stress, buoyancy , rms, a_fluc_b
-from .interpolation import velocities_to_center, yz_plane, xy_plane, vertical_line
-from .dense_plume import PlumeAnalysis
+from reader import OceananigansData
+from physics import reynolds_stress, buoyancy , rms, a_fluc_b
+from interpolation import velocities_to_center, yz_plane, xy_plane, vertical_line
+from dense_plume import PlumeAnalysis
 # Set up folder and simulation parameters
-folder = ''
+folder = '/Users/annapauls/Library/CloudStorage/OneDrive-UCB-O365/CU-Boulder/TESLa/Carbon Sequestration/Simulations/Oceananigans/NBP/salinity and temperature/no noise circle inlet/S0 = 0.1 dTdz = 0.01 MLD = 60 WENO mod'
+
 output_folder = os.path.join(folder, "plotting outputs") 
 name = "interp"
 
@@ -19,7 +19,6 @@ with_halos = False
 closure = False
 stokes = False
 salinity = True
-write_grid = False
 
 # flags for what to plot
 video = True
@@ -46,11 +45,12 @@ reader = OceananigansData(folder)
 reader.load_grid()
 x, y, z = reader.x, reader.y, reader.z
 nx = reader.nx
+lx = reader.lx
 dx = reader.dx
 hx = reader.hx
 # load time and equation of state info
-reader.load_time(reader)
-coeffs = reader.load_equation_of_state(reader, salinity)
+reader.load_time()
+coeffs = reader.load_equation_of_state(salinity)
 alpha = coeffs['alpha']
 if salinity:
     beta = coeffs['beta']
@@ -74,7 +74,8 @@ ranges['vel'] = [-1e-5, 1e-5]
 ranges['b'] = [-6.0*10**(-4), 6.0*10**(-4)]
 ranges['rho'] = [rho0-0.01, rho0+0.1]#0.1] # <--for stratification [rho0-0.01, rho0+0.1] # 
 ranges['rho_fluc'] = [-0.01, 0.01]
-ranges['Tracer'] = [0.0, 0.04]
+S_tol = 10**(-5)
+ranges['Tracer'] = [S_tol, 0.04]
 ranges['T'] = [T0-0.4, T0 + 0.005] # <--for stratification [T0-0.4, T0 + 0.005] # 
 ranges['u'] = [-6*10**(-3), 6*10**(-3)]
 ranges['v'] = [-6*10**(-3), 6*10**(-3)]
@@ -92,7 +93,7 @@ if xy_plot and salinity:
     xy_ranges['rho'] = [rho0-0.01, rho0+0.01]
     xy_ranges['Pdynamic'] = [-1*10**(-4), 1*10**(-4)]
     xy_ranges['T'] = [T0-0.05, T0+0.01]
-    xy_ranges['Tracer'] = [0.0, 0.01]
+    xy_ranges['Tracer'] = [S_tol, 0.01]
     xy_ranges['u'] = [-6*10**(-3), 6*10**(-3)]
     xy_ranges['v'] = xy_ranges['u']
 
@@ -127,12 +128,13 @@ for it, t in enumerate(reader.t_save):
     w = reader.lazy_field('w', t)
     if salinity:
         S = reader.lazy_field('S', t)
+        S[S<S_tol] = S_tol * 10**(-3)
     if xy_plot:
-        Pdynamic = reader.lazy_field('Pdynamic', t)
+        Pdynamic = reader.lazy_field('P_dynamic', t)
     # interpolate velocities to cell centers
     u, v, w = velocities_to_center(u, v, w)
     # convert temperature and salinity to buoyancy 
-    bs = buoyancy(T, rho0, coeffs, T0, g, tracer = S if salinity else None)
+    bs = buoyancy(T, rho0, coeffs, T0, tracer = S if salinity else None)
     b = bs['b_total']
     rho = bs['rho']
 
@@ -150,16 +152,17 @@ for it, t in enumerate(reader.t_save):
     if salinity:
         S_avg = np.mean(S, axis=(-3, -2))
         S_fluc = S - S_avg
-
-    # calcualte reynolds stresses
-    uw_fluc, uw_fluc_avg = reynolds_stress(u, w, u_avg, w_avg)
-    vw_fluc, vw_fluc_avg = reynolds_stress(v, w, v_avg, w_avg)
-
-    bu_fluc, bu_fluc_avg = reynolds_stress(b, u, b_avg, u_avg)
-    bv_fluc, bv_fluc_avg = reynolds_stress(b, v, b_avg, v_avg)
-    bw_fluc, bw_fluc_avg = reynolds_stress(b, w, b_avg, w_avg)
     
-    if turb_stat_flag or buoyancy_momentum_flag:
+    if turb_stat_flag or buoyancy_momentum_flag or buoyancy_analysis_flag:
+
+        # calcualte reynolds stresses
+        uw_fluc_avg = np.mean(reynolds_stress(u, w, u_avg, w_avg), axis=(-3, -2))
+        vw_fluc_avg = np.mean(reynolds_stress(v, w, v_avg, w_avg), axis=(-3, -2))
+
+        bu_fluc_avg = np.mean(a_fluc_b(b, u, b_avg, u_avg), axis=(-3, -2))
+        bv_fluc_avg = np.mean(a_fluc_b(b, v, b_avg, v_avg), axis=(-3, -2))
+        bw_fluc_avg = np.mean(a_fluc_b(b, w, b_avg, w_avg), axis=(-3, -2))
+
         uv_fluc_avg = np.mean(reynolds_stress(u, v, u_avg, v_avg), axis=(-3, -2))
         # rms fluctuations
         u_rms = rms(u)
@@ -174,11 +177,13 @@ for it, t in enumerate(reader.t_save):
     dbdz = np.gradient(b, z, axis=-1)
 
     if salinity:
-        centerx = 0.0
+        x0 = 0.0
         centery = 0.0
+        bw_fluc = a_fluc_b(b, w)
+        dense_plume.input_info(S, b_tracer = bs['b_C'], b_background = bs['b_T'], bw_fluc = bw_fluc)
         rp_profile = dense_plume.plume_tracer_radius(x, y)
-        S_fluc_center = vertical_line(S_fluc, x, y, centerx, centery)
-        T_fluc_center = vertical_line(T_fluc, x, y, centerx, centery)
+        S_fluc_center = vertical_line(S_fluc, x, y, x0, centery)
+        T_fluc_center = vertical_line(T_fluc, x, y, x0, centery)
 
     # buoyancy analysis 
     if buoyancy_analysis_flag or turb_stat_flag or buoyancy_momentum_flag:
@@ -194,10 +199,10 @@ for it, t in enumerate(reader.t_save):
         Ri = dense_plume.Ri
         z_neutral = dense_plume.neutral_layer(z)
 
-        w_center = vertical_line(w, x, y, centerx, centery)
-        bw_fluc_center = vertical_line(bw_fluc, x, y, centerx, centery)
-        rho_perturbed_center = vertical_line(rho_perturbed, x, y, centerx, centery)
-        b_center = vertical_line(b, x, y, centerx, centery)
+        w_center = vertical_line(w, x, y, x0, centery)
+        bw_fluc_center = vertical_line(bw_fluc, x, y, x0, centery)
+        rho_perturbed_center = vertical_line(rho_perturbed, x, y, x0, centery)
+        b_center = vertical_line(b, x, y, x0, centery)
         z_intrusion = dense_plume.max_penetration(z)
         w_intrusion = w_center[max_index]
         w_neutral = w_center[neutral_index]
@@ -239,19 +244,19 @@ for it, t in enumerate(reader.t_save):
         else:
             turb_stat_dir = turb_stats_plot(time, it, ranges, output_folder, lx, nx, z, mld, u_avg, v_avg, w_avg, u_rms, v_rms, w_rms, uv_fluc_avg, uw_fluc_avg, vw_fluc_avg, bu_fluc_avg, bv_fluc_avg, bw_fluc_avg, b_rms, rho_avg)
     if vert_slice_plot:
-        u_yz = yz_plane(u, x, centerx)
-        v_yz = yz_plane(v, x, centerx)
-        w_yz = yz_plane(w, x, centerx)
-        rho_yz = yz_plane(rho, x, centerx)
-        rho_perturbed_yz = yz_plane(rho_perturbed, x, centerx)
-        T_yz = yz_plane(T, x, centerx)
-        S_yz = yz_plane(S, x, centerx)
+        u_yz = yz_plane(u, x, x0)
+        v_yz = yz_plane(v, x, x0)
+        w_yz = yz_plane(w, x, x0)
+        rho_yz = yz_plane(rho, x, x0)
+        rho_perturbed_yz = yz_plane(rho_perturbed, x, x0)
+        T_yz = yz_plane(T, x, x0)
+        S_yz = yz_plane(S, x, x0)
         if it < 10:
             depths = np.array([-mld, ])
         else:
             neutral_depth = dense_plume.neutral_layer(z)
             depths = np.array([-mld, neutral_depth])
-        plane_slices_dir = vert_plane_slices(time[it], ranges, output_folder, lx, x, y, z, u_yz, v_yz, w_yz, rho_yz, rho_perturbed_yz, T = T_yz, S = S_yz, depths = depths)
+        plane_slices_dir = vert_plane_slices(time[it], it, ranges, output_folder, lx, x, y, z, u_yz, v_yz, w_yz, rho_yz, rho_perturbed_yz, T = T_yz, S = S_yz, depths = depths)
     if xy_plot and salinity:
         loc = "z = MLD"#"n = 230, z = " + str(np.round(z[230], 2)) + " m"
         loc_z = -mld
@@ -263,7 +268,7 @@ for it, t in enumerate(reader.t_save):
         Pdynamic_xy = xy_plane(Pdynamic, z, loc_z)
         T_xy = xy_plane(T, z, loc_z)
         S_xy = xy_plane(S, z, loc_z)
-        surface_dir = xy_plane_slices(time[it], xy_ranges, output_folder, x, y, u_xy, v_xy, w_xy, Pdynamic_xy, rho_xy, rho_perturbed_xy, loc, T = T_xy, S = S_xy)
+        surface_dir = xy_plane_slices(time[it], it, xy_ranges, output_folder, x, y, u_xy, v_xy, w_xy, Pdynamic_xy, rho_xy, rho_perturbed_xy, loc, T = T_xy, S = S_xy)
     if buoyancy_analysis_flag and not salinity:
         buoyancy_dir = buoyancy_analysis_plot(time[it], it, ranges, output_folder, lx, nx, z, x, z, mld, b_avg, w_avg, b_center, w_center, b_rms, bu_fluc_avg, bv_fluc_avg, bw_fluc_avg, b_fluc, rho_perturbed, Ri_avg, Ri_strat, Ri_plume, intrusion, neutral, w_neutral, w_intrusion, w_mld, rho_neutral, rho_intrusion, rho_perturbed_mld, bwfluc_neutral, bwfluc_intrusion, bwfluc_mld, alpha_vel, alpha_length, salinity)
     if buoyancy_analysis_flag and salinity: