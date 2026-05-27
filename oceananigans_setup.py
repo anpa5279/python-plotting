d80fe1c6c1fd0e27b2bb61e664da8aed37ffc703
anpa5279 <anpa5279@colorado.edu>
2026-05-27 10:50:45 -0600

    changing input info

--- oceananigans_setup.py
+++ oceananigans_setup.py
@@ -17,8 +17,8 @@ rms_flag = True # calculates RMS from 3D fields
 salinity = True
 
 # Set up folder and simulation parameters
-folder = '/Users/annapauls/Documents/TESLa /Simulations/Oceananigans/dense plume/salinity and temperature/no noise circle inlet/domain resolution testing/horizontal resolution/sj0.1-mld60-dTdz0.01-lx320-nx384'
-#'/glade/derecho/scratch/apauls/outputs/res-horizontal/sj0.1-mld60-dTdz0.01-lx320-nx192' #480-nx384
+folder = '/glade/derecho/scratch/apauls/outputs/res-horizontal/sj0.1-mld60-dTdz0.01-lx320-nx192' 
+#'/Users/annapauls/Documents/TESLa /Simulations/Oceananigans/dense plume/salinity and temperature/no noise circle inlet/domain resolution testing/horizontal resolution/sj0.1-mld60-dTdz0.01-lx320-nx384'
 file_path = os.path.join(folder, 'binning_rtz.h5')
 
 g = 9.80665
