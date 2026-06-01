8ac11c8fc2652287e9432bcdc6040f562fe5a403
anpa5279 <anpa5279@colorado.edu>
2026-06-01 09:37:09 -0600

    testing

--- oceananigans_setup.py
+++ oceananigans_setup.py
@@ -17,7 +17,7 @@ rms_flag = True # calculates RMS from 3D fields
 salinity = True
 
 # Set up folder and simulation parameters
-folder = '/glade/derecho/scratch/apauls/outputs/version109/horizontal-domain/coarse1'
+folder = '/glade/derecho/scratch/apauls/outputs/version109/horizontal-domain/mod-default'
 #'/Users/annapauls/Documents/TESLa /Simulations/Oceananigans/dense plume/salinity and temperature/no noise circle inlet/domain testing/Lz = 160m/S0 = 0.1 dTdz = 0.01 MLD = 60'
 print(f"Reading data from {folder}")
 file_path = os.path.join(folder, 'binning_rtz.h5')
