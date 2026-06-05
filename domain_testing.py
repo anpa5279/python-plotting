4738d4736dfcb454a4409a5e9fda4e7834f8c372
anpa5279 <anpa5279@colorado.edu>
2026-06-05 11:27:05 -0600

    correcting run script for derecho

--- domain_testing.py
+++ domain_testing.py
@@ -15,7 +15,8 @@ tracer_integral = True
 
 salinity = True
 # Set up folder and simulation parameters
-universal_folder = '/glade/derecho/scratch/apauls/outputs/version109/max-MLD/horizontal-domain/'
+universal_folder = '/Users/annapauls/Documents/TESLa /Simulations/Oceananigans/dense plume/salinity and temperature/no noise circle inlet/version109/default/horizontal domain/'
+#'/glade/derecho/scratch/apauls/outputs/version109/max-MLD/horizontal-domain/'
 #'/Users/annapauls/Documents/TESLa /Simulations/Oceananigans/dense plume/salinity and temperature/no noise circle inlet/version109/default/horizontal domain/'
 variations = 'horizontal resolution'
 cases_info = comparison_info(variations, universal_folder = universal_folder)
