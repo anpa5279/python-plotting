expanded binning verification by including inegrated area under the gaussian curve with ND. corrected fluctuation calculation of ur and utheta. in progress, removing rho from dimensional_analysis. domain_testing looks for stuff in file for mass. verified horizontal interpolation works. removed printing statements in interpolation and a simple flag fix. oceananigans_setup now has correct file paths. variable vs xt now exists. higher frequency output option to compare to field data is present and works.

binning_vertification.py
@@ -1,44 +1,58 @@
 import os
 import numpy as np
+import scipy
 import matplotlib.pyplot as plt
 
 from diagnostics import azimuthal_avg
 from plotting_general import plot_format, comparison_plot_opt
-
+show_field = False
 outdir = 'figures and videos/'
 os.makedirs(outdir, exist_ok=True)
 
 # domain information
 lx = 200, 200
-dx = 0.5, 0.5 
+dx = 0.125, 0.125 
 x = np.linspace(-lx[0]/2, lx[0]/2, int(lx[0]/dx[0])+1)
 y = np.linspace(-lx[1]/2, lx[1]/2, int(lx[1]/dx[1])+1)
 
 X, Y = np.meshgrid(x, y)
 n_gauss = [len(x), len(y)]
 
+# binning 
+d_min = np.max(dx)
+dr_bins = [2*d_min, 4*d_min, 8*d_min, 16*d_min, 32*d_min, 64*d_min, 128*d_min, 256*d_min]
+
 # create gaussian 
 def gaussian(a, x, y, sigma=20):
     return a*np.exp(-((x**2 + y**2) / (2 * sigma**2)))
 mag = 1
-gaus = gaussian(mag, X, Y)
-# binning 
-d_min = np.max(dx)
-dr_bins = [2*d_min, 4*d_min, 8*d_min, 16*d_min]
+s =128*d_min
+gaus = gaussian(mag, X, Y, sigma=s)
 r = []
 binned = []
-for dr in dr_bins:
+int_area = np.zeros(len(dr_bins))
+for i, dr in enumerate(dr_bins):
     r_temp, binned_temp = azimuthal_avg(gaus, X, Y, dx_scale=dr, return_r = True)
     r.append(r_temp)
     binned.append(binned_temp)
+    int_area[i] = np.sum(binned_temp)*dr 
+    1/2+1/(np.sqrt(2*np.pi))*np.sum(binned[-1])
 
 # plotting
 plot_format()
 color_opt, line_opt = comparison_plot_opt(len(binned)+1)
-fig, axes = plt.subplots(1, 3, figsize=(22, 7), gridspec_kw={'width_ratios': [0.75, 0.75, 1]})
+if show_field:
+    ncols = 4
+    field = 'with_field_'
+    width_opt = [0.75, 0.75, 0.75, 1]
+else:
+    ncols = 3
+    field = ''
+    width_opt = np.ones(ncols)
+fig, axes = plt.subplots(1, 3, figsize=(5*ncols+1, 5), gridspec_kw={'width_ratios': width_opt})
 axes = axes.ravel()
 for i, bin in enumerate(binned):
-    axes[0].scatter(r[i], bin, color=color_opt[i + 1], label=f'binning, dr = {dr_bins[i]} m', marker='x')
+    axes[0].scatter(r[i], bin, color=color_opt[i + 1], label=rf'dr/$\sigma$ = {dr_bins[i]/s}', marker='x')
 axes[0].plot(x[n_gauss[0]//2:], gaus[n_gauss[0]//2:, n_gauss[1]//2], color=color_opt[0], label=r'A$\cdot$exp$(-\frac{(x^2+y^2)}{2\sigma^2})$')
 axes[0].set_title("Field vs Binning")
 axes[0].set_xlim(0, x[-1])
@@ -47,26 +61,56 @@ axes[0].set_xlabel("r [m]")
 axes[0].set_ylim(0, mag*1.1)
 axes[0].set_ylabel(r'gaussian, A$\cdot$exp$(-\frac{(x^2+y^2)}{2\sigma^2})$')
 
+er_max = 0.0
 for i, bin in enumerate(binned):
     error = np.abs(bin - gaussian(mag, r[i], 0)) / mag * 100
-    axes[1].scatter(r[i], error, color=color_opt[i + 1], label=f'binning, dr = {dr_bins[i]} m', marker='x')
+    axes[1].scatter(r[i], error, color=color_opt[i + 1], label=rf'dr/$\sigma$ = {dr_bins[i]/s}', marker='x')
+    er_max = max(er_max, np.max(error))
 axes[1].set_title("Error")
 axes[1].set_xlim(0, x[-1])
-axes[1].legend()
+#axes[1].legend()
 axes[1].set_xlabel("r [m]")
-axes[1].set_ylim(0, 10)
+axes[1].set_ylim(0, er_max * 1.1)
 axes[1].set_ylabel(r"$\frac{\text{bin - gaussian}}{A}$[%]")
 
-im = axes[2].imshow(gaus, extent=[x[0], x[-1], y[0], y[-1]], interpolation ='none', aspect='auto')
-fig.colorbar(im, ax = axes[2], label='Variable magnitude', shrink=0.8)
-axes[2].set_title(r'A$\cdot$exp$(-\frac{(x^2+y^2)}{2\sigma^2})$')
-axes[2].set_xlim(x[0], x[-1])
-axes[2].set_xlabel("x [m]")
-axes[2].set_ylim(y[0], y[-1])
-axes[2].set_ylabel("y [m]")
-axes[2].set_aspect('equal')
+"""
+plotting log-log to get the order of accuracy of the error
+area = C * dr^p
+log(area) = log(C)+p*log(dr) --> equivalent to y = mx + b
+p is order of accuracy
+"""
+area_true = mag*np.sqrt(2*np.pi*s**2)/2 # because we are only looking at half of the gaussian
+def quadratic_fit(x, a, c):
+    return a*x**2 + c
+#dr_bins = np.insert(dr_bins, 0, 0)/s
+r_opt = np.linspace(0, max(dr_bins)*2, 1000)
+coef = np.polyfit(dr_bins, int_area/area_true, 1)
+axes[2].plot(r_opt, np.poly1d(coef)(r_opt), color=color_opt[0], label=rf'a={coef[0]:.2e}(dr/$\sigma$) + {coef[1]:.2f}', linestyle=line_opt[1], linewidth = 0.6)
+coef = scipy.optimize.curve_fit(quadratic_fit, dr_bins, int_area/area_true)[0]
+axes[2].plot(r_opt, quadratic_fit(r_opt, *coef), color=color_opt[0], label=rf'a={coef[0]:.2e}(dr/$\sigma$)$^2$ + {coef[1]:.2f}', linestyle=line_opt[2], linewidth = 0.6)
+
+for i, dr in enumerate(dr_bins):
+    axes[2].scatter(dr, int_area[i]/area_true, color=color_opt[i+1], marker='x')
+axes[2].set_title("Integrated Area")
+axes[2].set_xlim(min(dr_bins)*0.9, max(dr_bins)*1.1)
+axes[2].set_xlabel(r'dr/$\sigma$')
+axes[2].set_ylim(0.1, 1.1)#(min(int_area/area_true)*0.9, max(int_area/area_true)*1.05)
+axes[2].legend()
+axes[2].set_ylabel(r"ND Area, $\frac{\sum_{n=1}^{N} (\text{bin}(n)\cdot\text{dr})}{\frac{\text{A}}{2}\sqrt{2\pi\sigma^2}}$")
+axes[2].set_xscale('log')#, base=2)
+axes[2].set_yscale('log')#, base=2)
+
+if show_field:
+    im = axes[3].imshow(gaus, extent=[x[0], x[-1], y[0], y[-1]], interpolation ='none', aspect='auto')
+    fig.colorbar(im, ax = axes[3], label='Variable magnitude', shrink=0.8)
+    axes[3].set_title(r'A$\cdot$exp$(-\frac{(x^2+y^2)}{2\sigma^2})$')
+    axes[3].set_xlim(x[0], x[-1])
+    axes[3].set_xlabel("x [m]")
+    axes[3].set_ylim(y[0], y[-1])
+    axes[3].set_ylabel("y [m]")
+    axes[3].set_aspect('equal')
 
 # --- Save Frame ---
-frame_path = os.path.join(outdir, f"binning_verification_gaus.svg")
+frame_path = os.path.join(outdir, f"{field}binning_verification_gaus.svg")
 plt.savefig(frame_path)
 plt.close(fig)

diagnostics.py
@@ -6,7 +6,7 @@ import h5py
 from interpolation import velocities_to_center
 from physics import rms
 
-### -------------------------COLLECTING COMPARISON CASE INFO------------------------- ###
+### -------------------------COLLECTING COMPARISON CASE INFO----------------- ###
 def comparison_info(variations, universal_folder = '/Users/annapauls/Documents/TESLa /Simulations/Oceananigans/dense plume/salinity and temperature/no noise circle inlet', ND=False, name_uni = ''):
     wp = -0.001
     if variations == 'strat':
@@ -66,19 +66,23 @@ def comparison_info(variations, universal_folder = '/Users/annapauls/Documents/T
         mld = 60 * np.ones(num_cases)
         F_s = wp * 0.1 * np.ones(num_cases) 
     elif variations == 'vertical resolution':
-        folder_names =['nz = 64', 'nz = 128', 'nz = 192', 'nz = 256']
-        case_names =[r'$\Delta z = 1.5$m', r'$\Delta z = 0.75$m' r'$\Delta z = 0.5$m', r'$\Delta z = 0.375$m']
+        folder_names =['vert res/dx2', 
+                       'vert res/dx1', 
+                       '', 
+                       'vert res/dx025'
+                       ]
+        case_names =[r'$\Delta z = 2.0$m', r'$\Delta z = 1.0$m', r'$\Delta z = 0.5$m', r'$\Delta z = 0.25$m']
         num_cases = len(folder_names)
         dTdz = 0.01 * np.ones(num_cases) # background temperature gradient in K/m
         mld = 60 * np.ones(num_cases)
-        F_s = wp * 0.1 * np.ones(num_cases) 
+        F_s = wp * 0.1 * np.ones(num_cases)
     elif variations == 'AR=1':
         folder_names =['AR=1/dx2', 
                        'AR=1/dx1', 
                        '', 
-                       #'AR=1/dx025'
+                       'AR=1/dx025'
                        ]
-        case_names =[r'$\Delta x = 2.0$m', r'$\Delta x = 1.0$m', r'$\Delta x = 0.5$m']#, r'$\Delta x = 0.25$m']
+        case_names =[r'$\Delta x = 2.0$m', r'$\Delta x = 1.0$m', r'$\Delta x = 0.5$m', r'$\Delta x = 0.25$m']
         num_cases = len(folder_names)
         dTdz = 0.01 * np.ones(num_cases) # background temperature gradient in K/m
         mld = 60 * np.ones(num_cases)
@@ -141,7 +145,7 @@ def comparison_info(variations, universal_folder = '/Users/annapauls/Documents/T
             "F_s": F_s
         }
     return case_info
-### -------------------------BINNING------------------------- ###
+### -------------------------BINNING----------------------------------------- ###
 def azimuthal_avg(var, X, Y, dx_scale = None, return_r = False):
     if dx_scale is None:
         X_bin = X
@@ -177,7 +181,6 @@ def binning_oc(reader, center=(0.0, 0.0)):
     y = reader.y - center[1]
     z = reader.z
     X, Y, Z = np.meshgrid(x, y, z, indexing='ij')
-    dist = np.sqrt(X**2 + Y**2)
     nr = np.min(nx[:-1])//2
     S_rz = np.empty((nr, nx[2], nt))
     T_rz = np.empty((nr, nx[2], nt)) 
@@ -198,9 +201,8 @@ def binning_oc(reader, center=(0.0, 0.0)):
         v = velocities_to_center(v, axis=-2)
         w = velocities_to_center(w, axis=-1)
 
-        # u and v 
-        ur = u*X/dist + v*Y/dist
-        utheta = -u*Y/dist + v*X/dist
+        # u and v to cylindrical polar coordinates
+        ur, utheta = vel_to_cylindrical(u, v, X, Y)
 
         for k in range(nx[2]):
             if reader.salinity:
@@ -212,6 +214,12 @@ def binning_oc(reader, center=(0.0, 0.0)):
 
     return S_rz, T_rz, ur_rz, utheta_rz, w_rz
 
+### -------------------------CYLINDRICAL POLAR COORDINATES------------------- ###
+def vel_to_cylindrical(u, v, X, Y):
+    dist = np.sqrt(X**2 + Y**2)
+    ur = u*X/dist + v*Y/dist
+    utheta = -u*Y/dist + v*X/dist
+    return ur, utheta
 ### -------------------------CALCULATING 1D AVERAGES------------------------- ###
 def compute_temporal_averages(reader, start=10):
     # Load constants once
@@ -268,28 +276,32 @@ def compute_fluct_averages(reader):
     v = velocities_to_center(v, axis=-2)
     w = velocities_to_center(w, axis=-1)
 
+    # convert u and v to cylindrical polar coordinates (still lazy)
+    X, Y, Z = np.meshgrid(reader.x, reader.y, reader.z, indexing='ij')
+    ur, utheta = vel_to_cylindrical(u, v, X, Y)
+
     # Horizontal means over (nx, ny) → shape (nt, nz)
     T_xy = da.mean(T, axis=dims)
-    u_xy = da.mean(u, axis=dims)
-    v_xy = da.mean(v, axis=dims)
+    ur_xy = da.mean(ur, axis=dims)
+    utheta_xy = da.mean(utheta, axis=dims)
     w_xy = da.mean(w, axis=dims)
     b_xy = da.mean(b, axis=dims)
 
     # Fluctuation
     T_fluc = T - T_xy[:, np.newaxis, np.newaxis, :]
-    u_fluc = u - u_xy[:, np.newaxis, np.newaxis, :]
-    v_fluc = v - v_xy[:, np.newaxis, np.newaxis, :]
+    ur_fluc = ur - ur_xy[:, np.newaxis, np.newaxis, :]
+    utheta_fluc = utheta - utheta_xy[:, np.newaxis, np.newaxis, :]
     w_fluc = w - w_xy[:, np.newaxis, np.newaxis, :]
     b_fluc = b - b_xy[:, np.newaxis, np.newaxis, :]
 
     # averages of fluctuations
     T_fluc_avg = da.mean(T_fluc, axis=dims)
-    u_fluc_avg = da.mean(u_fluc, axis=dims)
-    v_fluc_avg = da.mean(v_fluc, axis=dims)
+    ur_fluc_avg = da.mean(ur_fluc, axis=dims)
+    utheta_fluc_avg = da.mean(utheta_fluc, axis=dims)
     w_fluc_avg = da.mean(w_fluc, axis=dims)
     b_fluc_avg = da.mean(b_fluc, axis=dims)
-    bu_fluc_avg = da.mean(b_fluc * u, axis=dims)
-    bv_fluc_avg = da.mean(b_fluc * v, axis=dims)
+    bur_fluc_avg = da.mean(b_fluc * ur, axis=dims)
+    butheta_fluc_avg = da.mean(b_fluc * utheta, axis=dims)
     bw_fluc_avg = da.mean(b_fluc * w, axis=dims)
     if reader.salinity:
         S_xy = da.mean(S, axis=dims)
@@ -297,21 +309,21 @@ def compute_fluct_averages(reader):
         S_fluc_avg = da.mean(S_fluc, axis=dims)
         return {'T_fluc': T_fluc_avg,
                 'S_fluc': S_fluc_avg,
-                'ur_fluc': u_fluc_avg,
-                'utheta_fluc': v_fluc_avg,
+                'ur_fluc': ur_fluc_avg,
+                'utheta_fluc': utheta_fluc_avg,
                 'w_fluc': w_fluc_avg,
                 'b_fluc': b_fluc_avg,
-                'bu_fluc': bu_fluc_avg,
-                'bv_fluc': bv_fluc_avg,
+                'bur_fluc': bur_fluc_avg,
+                'butheta_fluc': butheta_fluc_avg,
                 'bw_fluc': bw_fluc_avg}
     else:
         return {'T_fluc': T_fluc_avg,
-                'ur_fluc': u_fluc_avg,
-                'utheta_fluc': v_fluc_avg,
+                'ur_fluc': ur_fluc_avg,
+                'utheta_fluc': utheta_fluc_avg,
                 'w_fluc': w_fluc_avg,
                 'b_fluc': b_fluc_avg,
-                'bu_fluc': bu_fluc_avg,
-                'bv_fluc': bv_fluc_avg,
+                'bur_fluc': bur_fluc_avg,
+                'butheta_fluc': butheta_fluc_avg,
                 'bw_fluc': bw_fluc_avg}
 
 def compute_rms(reader):
@@ -331,7 +343,7 @@ def compute_rms(reader):
     return {'u_rms': u_rms,
             'v_rms': v_rms,
             'w_rms': w_rms}
-### -------------------------WRITING TEMPORAL AVERAGES------------------------- ###
+### -------------------------WRITING TEMPORAL AVERAGES------------------------ ###
 def write_temporal_averages(file_path, data):
     folder_contour = f"contour temporal averages"
 

dimensional_analysis.py
@@ -95,7 +95,6 @@ for i, reader in enumerate(readers):
         nz = np.max([nz, reader.nx[2]])
     if salinity and plot_1d_z:
         S_value = reader.load_S_temporal_avg('interp_temporal_averages.h5')
-        dense_plume.append(PlumeAnalysis(S_value*contour_bound))
 
 x = readers[0].x
 y = readers[0].y
@@ -122,10 +121,10 @@ plot_format()
 if plot_variables:
     if salinity:
         var_names = ['Tracer', 'Temperature', 'Density', 'u', 'v', 'w']#, 'Perturbed Vertical Buoyancy Flux', 'Perturbed Density']
-        range_names = ['Tracer', 'T', 'rho', 'u', 'v', 'w']#, 'bw_fluc', 'rho_fluc']
+        range_names = ['Tracer', 'T', 'b', 'u', 'v', 'w']#, 'bw_fluc', 'rho_fluc']
     else:
         var_names = ['Temperature', 'Density', 'u', 'v', 'w']#, 'Perturbed Vertical Buoyancy Flux', 'Perturbed Density']
-        range_names = ['T', 'rho', 'u', 'v', 'w']#, 'bw_fluc', 'rho_fluc']
+        range_names = ['T', 'b', 'u', 'v', 'w']#, 'bw_fluc', 'rho_fluc']
     planeslice = 'vertical' # 'vertical' or 'horizontal'
     variable_dir = {}
     if planeslice == 'horizontal':
@@ -140,7 +139,7 @@ if plot_variables:
 
 S_tol = 10**(-6)
 ranges = plot_ranges(lz = 96, mld = np.max(mld), rho0 = rho0, T0 = T0, dTdz = np.max(dTdz), C_tol = S_tol)
-ranges['rho'] = [rho0, rho0+0.15]
+ranges['b'] = [rho0, rho0+0.15]
 ranges['rho_fluc'] = [-0.025, 0.025]
 ranges['Tracer'] =[S_tol, 0.15]
 ranges['Tracer_fluc'] = [-0.2, 0.2]
@@ -289,7 +288,7 @@ else:
             u_plane = []
             v_plane = []
             w_plane = []
-            rho_plane = []
+            b_plane = []
             bw_plane = []
             rho_fluc_plane = []
         for i, reader in enumerate(readers):
@@ -308,7 +307,7 @@ else:
             w = velocities_to_center(w, axis=2)
             # convert temperature and salinity to buoyancy 
             b = buoyancy(reader)
-            rho_fluc = rho - np.mean(rho, axis=(-3, -2))
+            rho_fluc = b - np.mean(b, axis=(-3, -2))
 
             # calcualte buoyancy fluxes
             bu_fluc = a_fluc_b(b, u)
@@ -352,7 +351,7 @@ else:
                 u_plane.append(plane_slice_calc(u, x, x0))
                 v_plane.append(plane_slice_calc(v, x, x0))
                 w_plane.append(plane_slice_calc(w, x, x0))
-                rho_plane.append(plane_slice_calc(rho, x, x0))
+                b_plane.append(plane_slice_calc(b, x, x0))
                 if salinity:
                     S_plane.append(plane_slice_calc(S, x, x0))
             elif plot_variables and planeslice == 'horizontal':
@@ -361,7 +360,7 @@ else:
                     u_plane.append(plane_slice_calc(u, z, loc_z[i]))
                     v_plane.append(plane_slice_calc(v, z, loc_z[i]))
                     w_plane.append(plane_slice_calc(w, z, loc_z[i]))
-                    rho_plane.append(plane_slice_calc(rho, z, loc_z[i]))
+                    b_plane.append(plane_slice_calc(b, z, loc_z[i]))
                     if salinity:
                         S_plane.append(plane_slice_calc(S, z, loc_z[i]))
                 else:
@@ -369,7 +368,7 @@ else:
                     u_plane.append(u[:, :, n])
                     v_plane.append(v[:, :, n])
                     w_plane.append(w[:, :, n])
-                    rho_plane.append(rho[:, :, n])
+                    b_plane.append(b[:, :, n])
                     if salinity:
                         S_plane.append(S[:, :, n])
 
@@ -377,11 +376,11 @@ else:
     for it in nt:
         if plot_variables:
             if salinity: #'Tracer', 'T', 'Density', 'u', 'v', 'w'
-                variables = [S_plane, T_plane, rho_plane, u_plane, v_plane, w_plane]
+                variables = [S_plane, T_plane, b_plane, u_plane, v_plane, w_plane]
                 colorbar_labels = [r"g/kg", r"$^\circ$C", r"kg/m$^3$", r"m/s", r"m/s", r"m/s"]
                 cmaps = ['viridis', 'viridis', 'viridis', 'RdBu_r', 'RdBu_r', 'RdBu_r']
             else: #'T', 'Density', 'u', 'v', 'w'
-                variables = [T_plane, rho_plane, u_plane, v_plane, w_plane]
+                variables = [T_plane, b_plane, u_plane, v_plane, w_plane]
                 colorbar_labels = [r"$^\circ$C", r"kg/m$^3$", r"m/s", r"m/s", r"m/s"]
                 cmaps = ['viridis', 'viridis', 'RdBu_r', 'RdBu_r', 'RdBu_r', 'RdBu_r', 'RdBu_r']
             if planeslice == 'vertical':

domain_testing.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+import h5py
 import math
 import matplotlib.pyplot as plt
 import itertools
@@ -16,9 +17,9 @@ tracer_integral = True
 
 salinity = True
 # Set up folder and simulation parameters
-universal_folder = '/glade/derecho/scratch/apauls/outputs/version109/square-inlet/'
+universal_folder = '/Users/annapauls/Documents/TESLa /Simulations/Oceananigans/dense plume/salinity and temperature/no noise circle inlet/version109/res testing/square inlet/ground0'
 #'/Users/annapauls/Documents/TESLa /Simulations/Oceananigans/dense plume/salinity and temperature/no noise circle inlet/version109/default/horizontal domain/'
-variations = 'horizontal resolution'
+variations = 'AR=1'
 cases_info = comparison_info(variations, universal_folder = universal_folder)
 num_cases = cases_info['num_cases']
 case_names = cases_info['case_names']
@@ -77,18 +78,18 @@ for n, reader in enumerate(readers):
         dims = (1, 2, 3)
         S = reader.lazy_field('S').compute()
         # volume integral of S value in domain
-        S_int = np.mean(S, axis = dims)*vol
+        S_int = np.mean(S, axis = dims)*vol*rho0
     else:
-        domain = nx[0, n]//2*nx[2, n]
-        vol = np.pi*(lx[0, n]/2)**2*lx[2, n]
-        dims = (0, 1)
-        S = reader.load_binning_var('S')
-        r_bins = reader.loading_bin_radius()
-        # volume integral of S value in domain
-        S_int = np.mean(S*r_bins[:, None, None], axis = dims)*vol
+        file_path = os.path.join(reader.folder, 'binning_rtz.h5')
+        with h5py.File(file_path, 'r') as f:
+            S_int = f["mass/S"][:]
+            dmdt_reader = f["mass/dmdt"][:]
     if tracer_integral:
-        S_mass.append(S_int*rho0)
-        dmdt.append(np.gradient(S_mass[n], t[n]))
+        S_mass.append(S_int)
+        if "glade" in universal_folder:
+            dmdt.append(np.gradient(S_mass[n], t[n]))
+        else:
+            dmdt.append(dmdt_reader)
     if neg_tracer:
         S_neg = S
         S_neg[S>=0] = None
@@ -107,7 +108,7 @@ for n, reader in enumerate(readers):
         grid_area = Nr*reader.dx[0]*reader.dx[1]
         factor.append(area/grid_area)
         if tracer_integral:
-            S_mass[n] = S_int*rho0*factor[n]
+            S_mass[n] = S_int*factor[n]
             dmdt[n] = np.gradient(S_mass[n], t[n])
 
         if neg_tracer:
@@ -121,7 +122,7 @@ plot_format()
 if tracer_integral:
     scale = [1, 0.1]
     gridspec_kw={'height_ratios': scale}
-    fig, axes = plt.subplots(2, 2, figsize=(8, 6), dpi = 300, gridspec_kw = gridspec_kw, sharex = True)
+    fig, axes = plt.subplots(2, 2, figsize=(8, 6), dpi = 300, gridspec_kw = gridspec_kw)
     for a in axes[-1, :]:
             a.remove()
     axes = axes.ravel()

interpolation.py
@@ -41,7 +41,7 @@ def interp1d_axis(f, coord, f_new = None, coord_new = None, axis=-1):
         coord = np.asarray(coord)
 
         # find index below target
-        if f_new == 0:
+        if f_new.ndim==1 and f_new.all() == 0:
             idx = np.where(np.diff(np.sign(f))!=0)[0]#[0]
         else:
             idx = np.searchsorted(f, f_new) - 1
@@ -116,23 +116,20 @@ def point(f, z, f0 = None, z0 = None, x = None, x0 = 0.0, y = None, y0 = 0.0):
     Interpolate to a single point in space.
     time: if time is included in the matrix f, it should output a 1d array
     """
-    fnew = f
     if f0 is not None: # if field, inputs z, f
         znew = interp1d_axis(f, z, f_new = f0) 
         new = znew
-    if z0 is not None: # if field, inputs z, f
+    elif z0 is not None: # if field, inputs z, f
         fnew = interp1d_axis(f, z, coord_new = z0)
         new = fnew
+    else:
+        fnew = f 
+    
     if x is not None and y is not None: # if field, inputs x, y, z
         fnewy = interp1d_axis(fnew, y, coord_new = y0, axis= -2)
-        #print('fnewy', fnewy.shape)
         new = interp1d_axis(fnewy, x, coord_new = x0, axis = -3)
-        #print('new', new.shape)
     elif y is None and x is not None: # if field, inputs x, z, f
         new = interp1d_axis(fnew, x, coord_new = x0, axis = -3)
-        #print('new', new.shape)
     elif x is None and y is not None: # if field, inputs y, z, f
         new = interp1d_axis(fnew, y, coord_new = y0, axis= -2)
-        #print('new', new.shape)
-    #print('all if statements are done')
-    return new
+    return new.squeeze()

interpolation_verification.py
@@ -36,9 +36,11 @@ if video:
 else:
     time = reader.t[-1]
 
-nvars = 3
+labels = ['raw output', 'class function', 'averaging', 'point interpolation function']
+nvars = len(labels)
 if vertical_verification:
     fig_folder_vert = os.path.join(fig_folder, 'vertical_line')
+    os.makedirs(fig_folder_vert, exist_ok=True)
     zf = reader.zf
     # create necessary 3D fields
     w = reader.lazy_field('w').compute()
@@ -56,6 +58,7 @@ if vertical_verification:
                 w_interpolation[:, i, j, k] = point(w[:, i, j, :], zf, z0 = z[k])
 if horizontal_verification:
     fig_folder_horiz = os.path.join(fig_folder, 'horizontal_line')
+    os.makedirs(fig_folder_horiz, exist_ok=True)
     hml = 60
     hml_opt = np.argmin(np.abs(z + hml)) # approximate since we what to compare interpolation to raw output
     hml_opt = [hml_opt, hml_opt + 1]
@@ -64,26 +67,24 @@ if horizontal_verification:
     xf = reader.xf
     yf = reader.yf
     u = reader.lazy_field('u').compute()
+    u = u[:, :, hor_idx, hml_idx]
     v = reader.lazy_field('v').compute()
+    v = v[:, hor_opt, :, :][:, :, :, hml_opt]
     u_center_function = velocities_to_center(u, -3)
     v_center_function = velocities_to_center(v, -2)
-    u = np.concatenate([u, np.take(u, [0], axis=-3)], axis=-3)
-    v = np.concatenate([v, np.take(v, [0], axis=-2)], axis=-2)
-    u = u[:, :, hor_idx, hml_idx]
-    v = v[:, hor_idx, :, hml_idx]
-    u_averaging = (u[:, :-1, :, :] + u[:, 1:, :, :])/2
-    v_averaging = (v[:, :, :-1, :] + v[:, :, 1:, :])/2
-    """
+    uf = np.concatenate([u, np.take(u, [0], axis=-3)], axis=-3)
+    vf = np.concatenate([v, np.take(v, [0], axis=-2)], axis=-2)
+    u_averaging = (uf[:, :-1, :, :] + uf[:, 1:, :, :])/2
+    v_averaging = (vf[:, :, :-1, :] + vf[:, :, 1:, :])/2
     u_interpolation = np.empty_like(u_center_function)
     v_interpolation = np.empty_like(v_center_function)
-    for k in range(len(hml_opt)):
+    for k, hml_k in enumerate(hml_opt):
         for i in range(nx[0]):
             for j in range(len(hor_opt)):
-                u_interpolation[:, i, j, k] = point(u[:, :, j, k], xf, x0 = x[i])
+                u_interpolation[:, i, j, k] = point(uf[:, :, j, k][:, :, None, None], z[hml_idx], x = xf, x0 = x[i])
         for i in range(len(hor_opt)):
             for j in range(nx[1]):
-                v_interpolation[:, i, j, k] = point(v[:, i, :, k], yf, y0 = y[j])
-    """
+                v_interpolation[:, i, j, k] = point(vf[:, i, :, k][:, None, :, None], z[hml_k], y = yf, y0 = y[j])
 ############ PLOTTING ############
 """
     Plotting throughout time...
@@ -95,7 +96,6 @@ plot_format()
 os.makedirs(fig_folder, exist_ok=True)
 gridspec_kw={'height_ratios': [1, 1, 0.15]}
 width = 0.8
-labels = ['raw output', 'class function', 'averaging']#, 'point interpolation function']
 case_handles = [Line2D([0], [0], color=color_opt[i], linestyle='solid', linewidth=width, label=labels[i]) for i in range(nvars)]
 if vertical_verification:
     ncols = idx.size
@@ -139,12 +139,13 @@ if horizontal_verification:
         for ax in axes[-1, :]:
             ax.remove()
         for ax in axes[0, :]:
-            ax.set_xlim(-reader.lx[-1], 0)
+            ax.set_xlim(reader.x[0], reader.x[-1])
             ax.set_ylim(-0.15, 0.15)
             ax.set_xlabel("x [m]")
             ax.set_ylabel("u [m/s]")
+        
         for ax in axes[1, :]:
-            ax.set_xlim(-reader.lx[-1], 0)
+            ax.set_xlim(reader.y[0], reader.y[-1])
             ax.set_ylim(-0.15/10, 0.15/10)
             ax.set_xlabel("y [m]")
             ax.set_ylabel("v [m/s]")
@@ -158,19 +159,19 @@ if horizontal_verification:
         for j, jy in enumerate(hor_opt):
             for k, kz in enumerate(hml_opt):
                 axes[j + k + count].set_title(f"u at (Ny = {jy}, Nz = {kz})")
-                axes[j + k + count].plot(xf, u[it, :, j, k], label = labels[0], color = color_opt[0], linestyle = line_opt[0])
+                axes[j + k + count].plot(xf, uf[it, :, j, k], label = labels[0], color = color_opt[0], linestyle = line_opt[0])
                 axes[j + k + count].plot(x, u_center_function[it, :, j, k], label = labels[1], color = color_opt[1], linestyle = line_opt[1])
                 axes[j + k + count].plot(x, u_averaging[it, :, j, k], label = labels[2], color = color_opt[2], linestyle = line_opt[1])
-                #axes[j + k + count].plot(x, u_interpolation[it, :, j, k], label = labels[3], color = color_opt[3], linestyle = line_opt[1])
+                axes[j + k + count].plot(x, u_interpolation[it, :, j, k], label = labels[3], color = color_opt[3], linestyle = line_opt[1])
             count += 1
-        count += ix.size + jy.size
+        count += kz.size + jy.size
         for i, ix in enumerate(hor_opt):
             for k, kz in enumerate(hml_opt):
                 axes[i + k + count].set_title(f"v at (Nx = {ix}, Nz = {kz})")
                 axes[i + k + count].plot(yf, v[it, i, :, k], label = labels[0], color = color_opt[0], linestyle = line_opt[0])
                 axes[i + k + count].plot(y, v_center_function[it, i, :, k], label = labels[1], color = color_opt[1], linestyle = line_opt[1])
                 axes[i + k + count].plot(y, v_averaging[it, i, :, k], label = labels[2], color = color_opt[2], linestyle = line_opt[1])
-                #axes[i + k + count].plot(y, v_interpolation[it, i, :, k], label = labels[3], color = color_opt[3], linestyle = line_opt[1])
+                axes[i + k + count].plot(y, v_interpolation[it, i, :, k], label = labels[3], color = color_opt[3], linestyle = line_opt[1])
             count += 1
         # --- Save Frame ---
         frame_path = os.path.join(fig_folder_horiz, f"interpolation_testing_{it:04d}.png")

oceananigans_setup.py
@@ -26,7 +26,7 @@ if not salinity:
     mass_flag = False
 
 # Set up folder and simulation parameters
-folder = '/glade/derecho/scratch/apauls/outputs/version109/square-inlet/ground0/aspect-ratio/dx1=025/outputs'
+folder = '/glade/derecho/scratch/apauls/outputs/version109/square-inlet/vertical-res/dz1'
 
 print(f"Reading data from {folder}")
 bin_path = os.path.join(folder, 'binning_rtz.h5')
@@ -50,6 +50,7 @@ x, y, z = reader.x, reader.y, reader.z
 X, Y, Z = np.meshgrid(x, y, z)
 ncirc = min(nx[0], nx[1])//2      # full circular shells
 
+###------------APPLYING AZIMUTHAL AVERAGING TO DATA-----------------###
 if binning_flag:
     S_rz, T_rz, ur_rz, utheta_rz, w_rz = binning_oc(reader)
     # write to file 
@@ -81,7 +82,7 @@ if binning_flag:
             f.create_dataset("ccc/S", data=S_rz)
     del utheta_rz, ur_rz
     print(f"Saved binning to {bin_path}")
-
+###------------INTERPOLATION TO CENTERLINE--------------------------###
 if centerline_flag:
     file_path = os.path.join(folder, 'centerline.h5')
     T = reader.field_centerline('T')
@@ -108,7 +109,7 @@ if centerline_flag:
     del S, T, u, v, w
     print(f"Saved centerlines to {file_path}")
     reader.centerline = True
-
+###------------INTERPOLATION TO PLANESLICE--------------------------###
 if planelsice_flag:
     file_path = os.path.join(folder, 'plane_slice.h5')
     T = reader.field_slice('T')
@@ -137,7 +138,7 @@ if planelsice_flag:
     del S, T, u, v, w
     
     print(f"Saved plane slices to {file_path}")
-
+###------------BUOYANCY CALCULATIONS--------------------------------###
 if buoyancy_flag:
     buoyancy_file = os.path.join(folder, 'buoyancy_profile.h5')
     alpha = reader.alpha
@@ -173,7 +174,7 @@ if buoyancy_flag:
             f.create_dataset("centerline/b_fluc", data = b_fluc_centerline)
     del b_profile
     print(f"Saved buoyancy information to {buoyancy_file}")
-
+###------------FLUCTUATION AVERAGES---------------------------------###
 if fluc_flag:
     file_path = os.path.join(folder, 'fluctuations.h5')
     data = compute_fluct_averages(reader)
@@ -199,15 +200,15 @@ if fluc_flag:
         f.create_dataset("fluctuations/utheta_fluc", data=data['utheta_fluc'])
         f.create_dataset("fluctuations/w_fluc", data=data['w_fluc'])
         f.create_dataset("fluctuations/b_fluc", data=data['b_fluc'])
-        f.create_dataset("fluctuations/bur_fluc", data=data['bu_fluc'])
-        f.create_dataset("fluctuations/butheta_fluc", data=data['bv_fluc'])
+        f.create_dataset("fluctuations/bur_fluc", data=data['bur_fluc'])
+        f.create_dataset("fluctuations/butheta_fluc", data=data['butheta_fluc'])
         f.create_dataset("fluctuations/bw_fluc", data=data['bw_fluc'])
         if salinity:
             if "fluctuations/S_fluc" in f:
                 del f["fluctuations/S_fluc"]
             f.create_dataset("fluctuations/S_fluc", data=data['S_fluc'])
     print(f"Saved fluctuations to {file_path}")
-
+###------------ROOT MEAN SQUARE-------------------------------------###
 if rms_flag:
     file_path = os.path.join(folder, 'fluctuations.h5')
     rms_values = compute_rms(reader)
@@ -223,7 +224,7 @@ if rms_flag:
         f.create_dataset("rms/w", data=rms_values['w_rms'])
     del rms_values
     print(f"Saved RMS to {file_path}")
-
+###------------TEMPORAL AVERAGES------------------------------------###
 if compute_temporal_averages_flag:
     start = 10
     if reader.averaging:
@@ -237,7 +238,7 @@ if compute_temporal_averages_flag:
     write_temporal_averages(bin_path, data)
     del data_temp
     print(f"Saved temporal averages to {bin_path}")
-
+###------------PLUME CONTOURS---------------------------------------###
 if contour_flag:
     contours = np.array([0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05])
     S_value = reader.load_S_temporal_avg(bin_path)
@@ -297,7 +298,7 @@ if contour_flag:
             f.create_dataset(key, data=r_contour)
 
     print(f"Saved contours to {bin_path}")
-
+###------------MASS CALCULATIONS------------------------------------###
 if mass_flag:
     rho0 = 1026 # kg/m^3
     S = reader.lazy_field('S').compute()
@@ -308,7 +309,7 @@ if mass_flag:
     S_mass = np.mean(S, axis = dims)*vol*rho0
     del S
     dmdt = np.gradient(S_mass, time)
-    with h5py.File(file_path, "a") as f:
+    with h5py.File(bin_path, "a") as f:
         if "mass/S" in f:
             del f["mass/S"]
         if "mass/dmdt" in f:

oceananigans_simulation.py
@@ -6,7 +6,7 @@ from matplotlib.lines import Line2D
 
 from reader import OceananigansData
 from plotting_general import plot_format, create_video, comparison_plot_opt, plot_ranges
-from interpolation import point, vertical_line, interp1d_axis
+from interpolation import point, vertical_line, interp1d_axis, velocities_to_center
 """
     what is the best way to find the maximum penetration depth of a plume via momentum?
     ways to consider w:
@@ -25,8 +25,9 @@ from interpolation import point, vertical_line, interp1d_axis
         3. gradient changes
 """
 # plotting flags
+plot_xt = True
+plot_yt = False
 plot_zt = False
-plot_yt = True
 plot_raw_centerline = False
 plot_1dz_stats = False
 verify_outputs = False
@@ -44,20 +45,21 @@ if plot_1dz_stats:
 hml = 60
 g = 9.80665
 # collecting model information for all cases
+x = reader.x
 y = reader.y
 z = reader.z
 nx = reader.nx
 lx = reader.lx
 nt = reader.nt
 # video or not setup
-if video or plot_zt or plot_raw_centerline or plot_yt:
+if video or plot_zt or plot_raw_centerline or plot_yt or plot_xt:
     time = reader.t
     if reader.centerline:
         time1 = reader.time_center
 else:
     time = reader.t[-1]
-if plot_yt:
-    z_loc = -1.0*np.array([hml, hml+1, hml+2, hml+5, hml+10, hml+20]) #-[hml, ]
+if plot_yt or plot_xt:
+    z_loc = -1.0*np.array([hml, hml+1, hml+2, hml+5, hml+10])#, hml+20]) #-[hml, ]
 # load in information
 reader.load_equation_of_state()
 if plot_zt or verify_outputs:
@@ -65,6 +67,22 @@ if plot_zt or verify_outputs:
     b_avg, b_rms, b_centerline, b_fluc_centerline = reader.load_buoyancy_small()
     if reader.salinity:
         S_centerline = reader.field_centerline('S')
+    if verify_outputs:
+        w_centerline = w_centerline[::100, :]
+        b_centerline = b_centerline[::100, :]
+        reader.centerline = False
+        w_slice = reader.load_plane_var('w')
+        w_slice = velocities_to_center(w_slice, axis = -1)
+        w_coarse_centerline = vertical_line(w_slice, y = reader.y, y0 = 0.0)
+        T_slice = reader.load_plane_var('T')
+        T_coarse_centerline = vertical_line(T_slice, y = reader.y, y0 = 0.0)
+        b_coarse_centerline =  g * reader.alpha * (T_coarse_centerline - reader.T0)
+        del T_coarse_centerline
+        if reader.salinity:
+            S_centerline = S_centerline[::100, :]
+            S_slice = reader.load_plane_var('S')
+            S_coarse_centerline = vertical_line(S_slice, y = reader.y, y0 = 0.0)
+            b_coarse_centerline += - g * reader.beta * S_coarse_centerline
 if plot_1dz_stats or (plot_zt or verify_outputs):
     w_rms = reader.load_rms('w')
     u_r = reader.load_binning_var('horizontal velocity')
@@ -92,6 +110,29 @@ if plot_yt:
         b_yt += - g * reader.beta * S_yt
     b_fluc_yt = b_yt - b_avg_yt[:, None, :]
     del w_plane, S_plane, T_plane, b_yt
+
+if plot_xt:
+    buoyancy_file = os.path.join(reader.folder, 'buoyancy_profile.h5')
+    with h5py.File(buoyancy_file, 'r') as f:
+        b_avg = f['b_avg'][()]
+    w_plane = reader.field_slice('w', slice='XZ')
+    S_plane = reader.field_slice('S', slice='XZ')
+    T_plane = reader.field_slice('T', slice='XZ')
+    w_xt = np.empty((nt, len(x), len(z_loc)))
+    S_xt = np.empty((nt, len(x), len(z_loc)))
+    T_xt = np.empty((nt, len(x), len(z_loc)))
+    b_avg_xt = np.empty((nt, len(z_loc)))
+    for j, z_opt in enumerate(z_loc):
+        w_xt[:, :, j] = interp1d_axis(w_plane, z, coord_new = z_opt, axis = -1)
+        T_xt[:, :, j] = interp1d_axis(T_plane, z, coord_new = z_opt, axis = -1)
+        b_avg_xt[:, j] = point(b_avg, z, z0 = z_opt)
+        if reader.salinity:
+            S_xt[:, :, j] = interp1d_axis(S_plane, z, coord_new = z_opt, axis = -1)
+    b_xt = g * reader.alpha * (T_xt - reader.T0)
+    if reader.salinity:
+        b_xt += - g * reader.beta * S_xt
+    b_fluc_xt = b_xt - b_avg_xt[:, None, :]
+    del w_plane, S_plane, T_plane, b_xt
 print("finished loading data")
 # finding centerlines
 if plot_raw_centerline:
@@ -117,27 +158,18 @@ if plot_1dz_stats:
 
     dbrmsdz = np.gradient(b_rms, z, axis = -1)
     dbflucdz = np.gradient(b_fluc_centerline, z, axis = -1)
-if verify_outputs: # collecting coarser field output information to make sure it matches the higher frequency centerline and averaged outputs
-    reader.centerline_file = None
-    w_coarse_centerline = reader.field_centerline('w')
-    T_coarse_centerline = reader.field_centerline('T')
-    b_coarse_centerline =  g * reader.alpha * (T_coarse_centerline - reader.T0)
-    if reader.salinity:
-        S_coarse_centerline = reader.field_centerline('S')
-        b_coarse_centerline += - g * reader.beta * S_coarse_centerline
-    b_coarse_centerline = reader.field_centerline('b')
 
 ############ PLOTTING ############
 hml_var = np.arange(-10**2, 10**2)
 hml_array = -hml * np.ones(len(hml_var))
 ranges = plot_ranges()
-ranges['w'] = [-1.2*10**-1, 1.2*10**-1]
+ranges['w'] = [-1.8*10**-1, 1.8*10**-1]
 ranges['b_fluc'] = [-7*10**(-4), 7*10**(-4)]
 ranges['gradw'] = [-0.05, 0.05]
 ranges['gradb'] = [-0.0008, 0.0008]
 ranges['b_rms'] = [0, 1.5*10**(-5)]
 ranges['vel_rms'] = [0, 4*10**-3]
-ranges['S'] = [0.0, 0.05]
+ranges['S'] = [0.0, 0.1]
 factor = 10**(-2)
 nvars = 5
 color_opt, line_opt  = comparison_plot_opt(nvars)
@@ -211,7 +243,8 @@ if plot_zt:
     axes = axes.ravel()
     plt.subplots_adjust(bottom = 0.1, top = 0.95)
     for n, var in enumerate(vars):
-        im = axes[n].imshow(var.T, extent=[time.min()/(3600*24), time.max()/(3600*24), z.min(), z.max()], interpolation ='none', cmap=colors[n], vmin=range_opt[n][0], vmax=range_opt[n][1])
+        #np.flipud(var.T), var.T
+        im = axes[n].imshow(np.flipud(var.T), extent=[time.min()/(3600*24), time.max()/(3600*24), z.min(), z.max()], interpolation ='none', cmap=colors[n], vmin=range_opt[n][0], vmax=range_opt[n][1])
         axes[n].plot(time, -hml*np.ones_like(time), color = 'k', label=r"$\text{h}_{ML}$", linewidth = 0.9, linestyle = line_opt[1])
         axes[n].legend(loc='lower left')
         axes[n].set_xlim(time.min()/(3600*24), time.max()/(3600*24))
@@ -228,15 +261,16 @@ if plot_zt:
     plt.savefig(frame_path)
     plt.close(fig)
 if plot_yt:
+    w_max = np.max(np.abs(w_yt))
+    b_fluc_max = np.max(np.abs(b_fluc_yt))
     if reader.salinity:
+        range_opt = [(-w_max, w_max), (0.0, np.max(S_yt)), (np.min(T_yt), np.max(T_yt)), (-b_fluc_max, b_fluc_max)]
         titles = [r"w", r"S", r"T", r"b'", r"b"]
         colors = ['RdBu', 'Blues', 'viridis', 'RdBu']
         labels = ['[m/s]', '[g/kg]', r'[$^\circ$C]', r'[m/s$^2$]']
         vars = [w_yt, S_yt, T_yt, b_fluc_yt]
         file = 'wSTb_yt.svg'
     else:
-        w_max = np.max(np.abs(w_yt))
-        b_fluc_max = np.max(np.abs(b_fluc_yt))
         range_opt = [(-w_max, w_max), (np.min(T_yt), np.max(T_yt)), (-b_fluc_max, b_fluc_max)]
         titles = [r"w", r"T", r"b'", r"b"]
         colors = ['RdBu', 'viridis', 'RdBu']
@@ -245,12 +279,6 @@ if plot_yt:
         file = 'wTb_yt.svg'
     ncols = len(vars)
     for j, z_opt in enumerate(z_loc):
-        w_max = np.max(np.abs(w_yt[:, :, j]))
-        b_fluc_max = np.max(np.abs(b_fluc_yt[:, :, j]))
-        if reader.salinity:
-            range_opt = [(-w_max, w_max), (0.0, np.max(S_yt[:, :, j])), (np.min(T_yt[:, :, j]), np.max(T_yt[:, :, j])), (-b_fluc_max, b_fluc_max)]
-        else:
-            range_opt = [(-w_max, w_max), (np.min(T_yt[:, :, j]), np.max(T_yt[:, :, j])), (-b_fluc_max, b_fluc_max)]
         fig, axes = plt.subplots(1, ncols, figsize=(4*ncols, 5.5), sharex = True, sharey = True)
 
         ratio = ((time.max()/(3600*24))/lx[1])**-1
@@ -264,6 +292,46 @@ if plot_yt:
             axes[n].set_ylim(time.min()/(3600*24), time.max()/(3600*24))
             axes[n].set_xlabel("y [m]")
             axes[n].set_title(titles[n]+rf'(0, y, {z_opt})')
+            axes[n].set_aspect(ratio)
+            cbar = fig.colorbar(im, ax = axes[n], anchor = (0.5, 0.9), orientation='horizontal', label=labels[n], shrink=0.8, aspect=30)
+            cbar.formatter.set_useOffset(False)
+            cbar.formatter.set_powerlimits((-3, 5))
+            cbar.update_ticks() 
+        frame_path = os.path.join(outdir, rf'z{z_loc[j]}_{file}')
+        plt.savefig(frame_path)
+        plt.close(fig)
+if plot_xt:
+    w_max = np.max(np.abs(w_xt))
+    b_fluc_max = np.max(np.abs(b_fluc_xt))
+    if reader.salinity:
+        range_opt = [(-w_max, w_max), (0.0, np.max(S_xt)), (np.min(T_xt), np.max(T_xt)), (-b_fluc_max, b_fluc_max)]
+        titles = [r"w", r"S", r"T", r"b'", r"b"]
+        colors = ['RdBu', 'Blues', 'viridis', 'RdBu']
+        labels = ['[m/s]', '[g/kg]', r'[$^\circ$C]', r'[m/s$^2$]']
+        vars = [w_xt, S_xt, T_xt, b_fluc_xt]
+        file = 'wSTb_xt.svg'
+    else:
+        range_opt = [(-w_max, w_max), (np.min(T_xt), np.max(T_xt)), (-b_fluc_max, b_fluc_max)]
+        titles = [r"w", r"T", r"b'", r"b"]
+        colors = ['RdBu', 'viridis', 'RdBu']
+        labels = ['[m/s]', r'[$^\circ$C]', r'[m/s$^2$]']
+        vars = [w_xt, T_xt, b_fluc_xt]
+        file = 'wTb_xt.svg'
+    ncols = len(vars)
+    for j, z_opt in enumerate(z_loc):
+        fig, axes = plt.subplots(1, ncols, figsize=(4*ncols, 5.5), sharex = True, sharey = True)
+
+        ratio = ((time.max()/(3600*24))/lx[1])**-1
+        axes = axes.ravel()
+        fig.suptitle(f"z = {z_opt} m")
+        plt.subplots_adjust(bottom = 0.1, top = 0.9)
+        axes[0].set_ylabel("time [days]")
+        for n, var in enumerate(vars):
+            im = axes[n].imshow(var[:, :, j], extent=[x.min(), x.max(), time.min()/(3600*24), time.max()/(3600*24)], interpolation ='none', cmap=colors[n], vmin=range_opt[n][0], vmax=range_opt[n][1])
+            axes[n].set_xlim(x.min(), x.max())
+            axes[n].set_ylim(time.min()/(3600*24), time.max()/(3600*24))
+            axes[n].set_xlabel("x [m]")
+            axes[n].set_title(titles[n]+rf'(x, 0, {z_opt})')
             axes[n].set_aspect(ratio)
             cbar = fig.colorbar(im, ax = axes[n], anchor = (0.5, 0.9), orientation='horizontal', label=labels[n], shrink=0.8, aspect=30)
             cbar.formatter.set_useOffset(False)
@@ -358,7 +426,45 @@ if plot_1dz_stats:
         frame_path = os.path.join(fig_var_folder, f"variables_of_interest_{it:04d}.png")
         plt.savefig(frame_path)
         plt.close(fig)
+if verify_outputs:
+    outdir_verify = os.path.join(outdir, 'verify_outputs')
+    os.makedirs(outdir_verify, exist_ok=True)
+    width = 0.8
+    for it in range(nt):
+        td = time[it]/(3600*24)
+        fig, axes = plt.subplots(1, 3, figsize=(15, 5), sharey = True)
+        axes = axes.ravel()
+        plt.subplots_adjust(top = 0.9)
+        fig.suptitle(f"t = {td:.2f} days")
+        axes[0].plot(w_centerline[it, :], z, color = 'k', linewidth = width, label='high frequency')
+        axes[0].plot(w_coarse_centerline[it, :], z, color = 'b', linewidth = width/2, linestyle = line_opt[1], label='coarse output')
+        axes[0].set_title("w")
+        axes[0].set_xlabel("[m/s]")
+        axes[0].set_xlim(ranges['w'])
+        axes[0].set_ylabel("z [m]")
+        axes[0].legend(loc='lower left')
 
+        axes[1].plot(b_centerline[it, :], z, color = 'k', linewidth = width, label='high frequency')
+        axes[1].plot(b_coarse_centerline[it, :], z, color = 'b', linewidth = width/2, linestyle = line_opt[1], label='coarse output')
+        axes[1].set_title("b")
+        axes[1].set_xlabel(r"[m/s$^2$]")
+        axes[1].set_xlim(ranges['b'])
+        axes[1].legend(loc='upper left')
+
+        if reader.salinity:
+            axes[2].plot(S_centerline[it, :], z, color = 'k', linewidth = width, label='high frequency')
+            axes[2].plot(S_coarse_centerline[it, :], z, color = 'b', linewidth = width/2, linestyle = line_opt[1], label='coarse output')
+            axes[2].set_title("S")
+            axes[2].set_xlabel(r"[g/kg]")
+            axes[2].set_xlim(ranges['S'])
+            axes[2].legend(loc='lower right')
+
+        frame_path = os.path.join(outdir_verify, f'verify_outputs_{it}.png')
+        plt.savefig(frame_path)
+        plt.close(fig)
 # creating videos
 if video:
-    create_video(fig_var_folder, outdir, '', 'max penetration variables')
+    if plot_1dz_stats:
+        create_video(fig_var_folder, outdir, '', 'max penetration variables')
+    if verify_outputs:
+        create_video(outdir_verify, outdir, '', 'verify_outputs')