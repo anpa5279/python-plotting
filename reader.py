51f9d41246d51cc9c32a8cc2d0f08121cae31095
anpa5279 <anpa5279@colorado.edu>
2026-07-20 15:44:37 -0600

    printing statements for debugging

--- reader.py
+++ reader.py
@@ -295,6 +295,7 @@ class OceananigansData:
             'XY': (self.z if coord_opt['z'] == 'c' else self.zf, False, -1),
         }
         coord, rank_split, axis = slice_cfg[plane]
+        print(f"coord: {coord}, rank_split: {rank_split}, axis: {axis}")
         exact = coord == loc
         if any(exact):
             needs_interp = False
